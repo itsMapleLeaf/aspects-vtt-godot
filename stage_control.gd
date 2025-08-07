122df493bc9cd00b55ad2fc24d196daae8f0cfb1
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-08-07 18:22:00 -0500

    separate camera class

--- stage_control.gd
+++ stage_control.gd
@@ -2,7 +2,6 @@ extends Control
 
 @onready var stage: Stage = %Stage
 
-
 func _gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.is_pressed():
@@ -11,15 +10,15 @@ func _gui_input(event: InputEvent) -> void:
 					stage.select_actors([])
 					get_viewport().set_input_as_handled()
 				MOUSE_BUTTON_WHEEL_UP:
-					stage.update_zoom(1, event.position)
+					stage.camera.update_zoom(1, event.position)
 					get_viewport().set_input_as_handled()
 				MOUSE_BUTTON_WHEEL_DOWN:
-					stage.update_zoom(-1, event.position)
+					stage.camera.update_zoom(-1, event.position)
 					get_viewport().set_input_as_handled()
 
 	elif event is InputEventMouseMotion:
 		if event.button_mask & MOUSE_BUTTON_MASK_RIGHT:
-			stage.offset += event.relative
+			stage.camera.offset += event.relative
 			get_viewport().set_input_as_handled()
 
 	elif event is InputEventPanGesture:
@@ -27,7 +26,7 @@ func _gui_input(event: InputEvent) -> void:
 		get_viewport().set_input_as_handled()
 
 	elif event is InputEventMagnifyGesture:
-		stage.update_zoom(event.factor, event.position)
+		stage.camera.update_zoom(event.factor, event.position)
 		get_viewport().set_input_as_handled()
 
 func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
@@ -38,5 +37,5 @@ func _drop_data(at_position: Vector2, data_arg: Variant) -> void:
 
 	stage.add_actor(
 		data.asset.get_image(),
-		(at_position - stage.offset) / stage.scale,
+		(at_position - stage.camera.offset) / stage.camera.zoom_scale,
 	)
