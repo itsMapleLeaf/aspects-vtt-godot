c639974a4c0b2182263e25e93761a46982cc30b7
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-08-07 18:34:36 -0500

    return to former dropzone/stage setup
    fixes an issue where the assets within the former stage control took inputs from the resources panel because it comes after in scene order, even if it's behind the panel visually
    
    basically organizing my tree in a way that hopefully requires less dumb hacky workarounds

--- stage.gd
+++ stage.gd
@@ -5,6 +5,45 @@ const ActorScene = preload("res://actor.tscn")
 
 var camera := StageCamera.new()
 
+func _process(delta: float) -> void:
+	transform = transform.interpolate_with(
+		Transform2D()
+			.scaled(camera.zoom_coefficient ** camera.zoom_tick * Vector2.ONE)
+			.translated(camera.offset),
+		delta * camera.stiffness,
+	)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			match event.button_index:
+				MOUSE_BUTTON_LEFT:
+					select_actors([])
+					get_viewport().set_input_as_handled()
+				MOUSE_BUTTON_WHEEL_UP:
+					camera.update_zoom(1, event.position)
+					get_viewport().set_input_as_handled()
+				MOUSE_BUTTON_WHEEL_DOWN:
+					camera.update_zoom(-1, event.position)
+					get_viewport().set_input_as_handled()
+
+	elif event is InputEventMouseMotion:
+		if event.button_mask & MOUSE_BUTTON_MASK_RIGHT:
+			camera.offset += event.relative
+			get_viewport().set_input_as_handled()
+
+	elif event is InputEventPanGesture:
+		position += event.delta
+		get_viewport().set_input_as_handled()
+
+	elif event is InputEventMagnifyGesture:
+		camera.update_zoom(event.factor, event.position)
+		get_viewport().set_input_as_handled()
+
+
+func _on_actor_pressed(actor: Actor) -> void:
+	select_actors([actor])
+
 
 func add_actor(image: Image, position: Vector2) -> void:
 	var actor: Actor = ActorScene.instantiate()
@@ -18,12 +57,3 @@ func select_actors(actors: Array[Actor]) -> void:
 	for actor in get_children():
 		if actor is Actor:
 			actor.is_selected = actors.has(actor)
-
-
-func _process(delta: float) -> void:
-	transform = transform.interpolate_with(
-		Transform2D()
-			.scaled(camera.zoom_coefficient ** camera.zoom_tick * Vector2.ONE)
-			.translated(camera.offset),
-		delta * camera.stiffness,
-	)
